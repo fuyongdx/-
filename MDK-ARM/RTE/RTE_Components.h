6fb26203bc413526f38ecf1a0f6a753ca364cfcd
fuyong <693380367@qq.com>
2022-04-07 21:51:59 +0800

    test commit

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'METER' 
+ * Target:  'METER' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
